88265e748153d1d38abd6ba487583d64e82413e3
Legrain Samuel <samuel.legrain@bstorm.be>
2022-04-27 11:16:20 +0200

    first commit

---
+++ Demos/Demo01 - Integration/myFirstScript.js
@@ -0,0 +1,21 @@
+// Premier code
+alert('Hello world!');
+
+/**
+ * Voici un premier script réalisé pour le cours de
+ * Javascript donné le 27 avril 2022!
+ * Qu'il est joli ce script!
+ */
+
+console.log('Affiche un message');
+console.warn('Affiche un avertissement');
+console.error('Affiche une erreur');
+//Affiche une table de manière propre
+console.table([1,2,3,4,5]);
+
+//Affiche le document HTML
+console.log(document);
+//Affiche les propriétés du document HTML
+console.dir(document);
+
+console.assert(false, 'Affiche un message d\'erreur si le booléen est false.')
